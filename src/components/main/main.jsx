51bb50a53d023d91053906e39f18b282b059c0d1
shadowctrl <shadowctrl@outlook.com>
2023-06-08 23:36:52 +0530

    Function Call Projects

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import anonymous from "../../assets/anonymous.png";
 import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
-import Project from "../projects/project.jsx";
+import { Project } from "../projects/project.jsx";
 import Skills from "../skills/skills";
 import Aboutme from "../aboutme/aboutme";
 import Contacts from "../contacts/contacts";
@@ -43,9 +43,47 @@ const Main = () => {
           <FormatQuoteIcon />
         </FormatQuoteBottom>
         <SideBox />
-        <Project />
+        {Project("Projects")}
         <Skills />
-        <Aboutme />
+        {/* <Aboutme /> */}
+        <Head>
+          <span>#</span>About-Me
+          <Dash />
+        </Head>
+        <Container>
+          <Border>
+            <span>S</span>
+            <span>H</span>
+            <span>A</span>
+            <span>D</span>
+            <span>O</span>
+            <span>W</span>
+            <span>C</span>
+            <span>T</span>
+            <span>R</span>
+            <span>L</span>
+          </Border>
+          <About>
+            As a highly motivated MERN stack developer with expertise in Discord
+            API, Google API, Firebase, and Cyber Security, I am excited to offer
+            my services as a freelancer. With a proven track record of building
+            robust and scalable applications using cutting-edge technologies, I
+            am confident that I can deliver high-quality work to clients with
+            diverse needs.
+            <br /> <br />
+            Whether it's building a custom web application, integrating
+            third-party APIs, or managing a database on Firebase, I have the
+            skills and experience to get the job done. As a Cyber Security
+            graduate, I understand the importance of building secure and
+            resilient applications, and am committed to implementing industry
+            best practices in all my projects.
+            <br /> <br />
+            With excellent communication skills, a strong work ethic, and a
+            dedication to delivering projects on time and within budget, I am
+            confident that I can exceed your expectations as a freelancer.
+          </About>
+          <img src="/images/dp.jpg" alt="Shadowctrl" />
+        </Container>
         <Contacts />
       </Main2>
     </Parent>
@@ -162,4 +200,57 @@ const SideBox = styled.div`
   padding: 2vw;
   margin-top: 10vh;
   margin-left: 90vw;
+`;
+
+const Head = styled.div`
+  display: flex;
+  align-items: center;
+  color: white;
+  font-family: Fira Code;
+  margin-left: 10vw;
+  margin-top: 5vh;
+  font-size: 2vw;
+  font-weight: 600;
+  span {
+    color: #c778dd;
+  }
+`;
+const Dash = styled.div`
+  /* position: absolute; */
+  border-top: 1px solid #c778dd;
+  width: 36vw;
+  margin-left: 2vw;
+`;
+
+const Border = styled.div`
+  border: 1px solid #bdbdbd;
+  padding: 0.5vw;
+  writing-mode: vertical-lr;
+  text-orientation: upright;
+  text-align: center;
+  font-size: 1vw;
+  color: #c778dd;
+  span {
+    font-family: Source Code Pro;
+    padding-top: 1.5vh;
+  }
+`;
+const Container = styled.div`
+  display: flex;
+  margin-right: 10vw;
+  img {
+    width: 15vw;
+    /* height: 55vh; */
+    border-radius: 1vw;
+    opacity: 0.8;
+    box-shadow: 2vw 2vh 2vw;
+  }
+`;
+const About = styled.div`
+  padding-left: 7.5vw;
+  padding-top: 5vh;
+  padding-right: 4vw;
+  text-align: justify;
+  color: #abb2bf;
+  font-family: Fira Code;
 `;
