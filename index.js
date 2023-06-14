e87a471bc01c525e9600d4166238662ec7e8c9d8
shadowctrl <shadowctrl@outlook.com>
2023-06-14 21:15:54 +0530

    IP domain redirect

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import express from "express";
+import https from "https";
+import fs from "fs";
 import path from "path";
-const __dirname = path.resolve();
+
 const app = express();
 const port = 443;
-app.use(express.static(path.resolve(__dirname, "dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "dist/index.html"));
+const __dirname = path.resolve();
+
+app.use((req, res, next) => {
+  const { headers, hostname, originalUrl } = req;
+  console.log(req.socket.remoteAddress);
+  if (/^\d+\.\d+\.\d+\.\d+$/.test(hostname)) {
+    const domain = "shadowctrl.me";
+    const redirectTo = `https://${domain}/`;
+
+    return res.redirect(301, redirectTo);
+  }
+
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Up and running on port: ${port}`);
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, "credentials/private.pem")),
+  cert: fs.readFileSync(path.resolve(__dirname, "credentials/public.pem")),
+};
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`Started execution on port ${port}`);
 });
