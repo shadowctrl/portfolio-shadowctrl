b15700558d9c11a941c248a8de7ec863284d8b77
shadowctrl <shadowctrl@outlook.com>
2023-06-17 07:19:04 +0530

    Sitemap Generator

---
+++ sitemap.cjs
@@ -0,0 +1,36 @@
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { createReadStream, createWriteStream } = require("fs");
+const { Readable } = require("stream");
+const { StaticRouter } = require("react-router-dom");
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
+
+const renderToString = (element) =>
+  ReactDOMServer.renderToString(React.createElement(element));
+
+async function generateSitemap() {
+  const staticRoutes = ["/", "Home", "About Me", "/Services", "/Contact Us"];
+
+  const smStream = new SitemapStream({
+    hostname: "https://www.shadowctrl.me",
+  });
+
+  staticRoutes.forEach((route) => {
+    smStream.write({
+      url: route,
+      changefreq: "daily",
+      priority: 0.7,
+    });
+  });
+
+  smStream.end();
+
+  const sitemapXml = await streamToPromise(Readable.from(smStream), {
+    encoding: "utf-8",
+  });
+
+  const writeStream = createWriteStream("./public/sitemap.xml");
+  writeStream.write(sitemapXml);
+  writeStream.end();
+}
+generateSitemap();
