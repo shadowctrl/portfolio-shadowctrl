790be15bf097195bfcc472db915a9814197c2283
shadowctrl <shadowctrl@outlook.com>
2023-06-20 17:12:33 +0530

    Helmet

--- src/App.jsx
+++ src/App.jsx
@@ -10,76 +10,79 @@ import Contactus from "./components/contactus/contactus";
 import Navbar from "./components/navbar/navbar";
 import Aboutme from "./components/aboutme/aboutme";
 import Services from "./components/services/services";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 const App = () => {
   return (
     <React.StrictMode>
-      <Helmet>
-        <meta property="og:image" content={"/images/og.jpg"} />
-      </Helmet>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<Error />} />
-          <Route
-            path="/"
-            element={
-              <>
-                <Navbar />
-                <Main />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path="/Home"
-            element={
-              <>
-                <Navbar />
-                <Main />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path="/Works"
-            element={
-              <>
-                <Navbar /> <Works /> <Footer />
-              </>
-            }
-          />
+      <HelmetProvider>
+        <Helmet>
+          <meta property="og:image" content={"/images/og.jpg"} />
+        </Helmet>
 
-          <Route
-            path="Contact"
-            element={
-              <>
-                <Navbar />
-                <Contactus /> <Footer />
-              </>
-            }
-          />
-          <Route
-            path="About Me"
-            element={
-              <>
-                <Navbar />
-                <Aboutme />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path="Services"
-            element={
-              <>
-                <Navbar />
-                <Services />
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<Error />} />
+            <Route
+              path="/"
+              element={
+                <>
+                  <Navbar />
+                  <Main />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/Home"
+              element={
+                <>
+                  <Navbar />
+                  <Main />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/Works"
+              element={
+                <>
+                  <Navbar /> <Works /> <Footer />
+                </>
+              }
+            />
+
+            <Route
+              path="Contact"
+              element={
+                <>
+                  <Navbar />
+                  <Contactus /> <Footer />
+                </>
+              }
+            />
+            <Route
+              path="About Me"
+              element={
+                <>
+                  <Navbar />
+                  <Aboutme />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="Services"
+              element={
+                <>
+                  <Navbar />
+                  <Services />
+                  <Footer />
+                </>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </HelmetProvider>
     </React.StrictMode>
   );
 };
