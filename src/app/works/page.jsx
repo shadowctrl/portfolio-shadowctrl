3ae53f539be5e984f7dc15cdc12f6e1a0c650caf
shadowctrl <shadowctrl@outlook.com>
2024-03-12 17:01:14 +0530

    Works Page

---
+++ src/app/works/page.jsx
@@ -0,0 +1,129 @@
+import "./works.css";
+import projects from "./projects";
+import { onGoingProjects } from "./projects";
+import Image from "next/image";
+import Link from "next/link";
+
+export const OnGoingWorks = (Title) => {
+  return (
+    <div>
+      <div className="worksChild2">
+        <span>#</span>Currently Working On
+      </div>
+      <div className="worksProjectsRenderer">
+        {onGoingProjects.map((value, index) => (
+          <div key={value.title} className="worksProjectContainer">
+            <div className="worksProjectImage">
+              <Image
+                src={value.image}
+                alt="Projects"
+                width="500"
+                height="500"
+              />
+            </div>
+            <div className="worksProjectLang">{value.languages}</div>
+            <div className="worksProjectTitle">{value.title}</div>
+            <div className="worksProjectDes">{value.description}</div>
+            <div className="worksProjectButtons">
+              {value.live ? (
+                <div className="worksProjectLiveButton">
+                  <Link href={value.live}>
+                    Live
+                    {/* <ArrowOutwardSharpIcon
+                      style={{ fontSize: "1vw", marginLeft: "0.3vw" }}
+                    /> */}
+                    <span className="worksArrowIcon"></span>{" "}
+                  </Link>
+                </div>
+              ) : (
+                ""
+              )}
+
+              {value.progress ? (
+                <div className="works-project-cached-button">
+                  <Link href={value.progress} target="_blank">
+                    Progress
+                    <span className="arrow-icon">
+                      {/* <ArrowOutwardSharpIcon
+                        style={{ fontSize: "1vw", marginLeft: "0.3vw" }}
+                      /> */}
+                    </span>
+                  </Link>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const works = () => {
+  return (
+    <div className="worksParent">
+      <div className="worksHead">
+        <span>/</span>Projects
+      </div>
+
+      <div className="worksChild">
+        <span>#</span>Completed Projects
+      </div>
+
+      <div className="worksProjectMain">
+        {/* <Box1 />
+    <Box /> */}
+        <div className="worksProjectsRenderer">
+          {projects.map((value, index) => (
+            <div className="worksProjectContainer" key={value.title}>
+              <div className="worksProjectImage">
+                <Image
+                  src={value.image}
+                  alt="Projects"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="worksProjectLang">{value.languages}</div>
+              <div className="worksProjectTitle">{value.title}</div>
+              <div className="worksProjectDes">{value.description}</div>
+              <div className="worksProjectButtons">
+                {value.live ? (
+                  <button className="worksProjectLiveButton">
+                    <Link href={value.live} target="_blank">
+                      {" "}
+                    </Link>
+                    Live{" "}
+                    {/* <ArrowOutwardSharpIcon
+                      style={{ fontSize: "1vw", marginLeft: "0.3vw" }}
+                    /> */}
+                  </button>
+                ) : (
+                  ""
+                )}
+
+                {value.cached ? (
+                  <button className="worksProjectCachedButton">
+                    <Link href={value.cached} target="_blank">
+                      Cached{" "}
+                      {/* <AutorenewSharp
+                        style={{ fontSize: "1vw", marginLeft: "0.3vw" }}
+                      /> */}
+                    </Link>
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <OnGoingWorks />
+    </div>
+  );
+};
+
+export default works;
---
+++ src/app/works/projects.js
@@ -0,0 +1,60 @@
+const projects = [
+  {
+    image: "/assets/hackatank.webp",
+    languages: "ReactJS, Html, Css",
+    title: "Hackathon Website",
+    description: "Showcasing my national-level, 24-hour hackathon website.",
+    live: "https://www.hackatank.tech",
+    cached: "https://github.com/shadowctrl/HackATank-Website",
+  },
+  {
+    image: "/assets/pepper.webp",
+    languages: "NodeJS, MongoDB, Lavalink, Discord API",
+    title: "Verified Discord Bot",
+    description: "A officially Verified Discord Music Application",
+    live: "https://pepperbot.muralianand.in/",
+  },
+  {
+    image: "/assets/csi.webp",
+    languages: "Reactjs, Html, Css",
+    title: "CSI International Conference",
+    description: "NGTCA 2023 website",
+    live: "https://icngt2023.org",
+  },
+  {
+    image: "/assets/monero.webp",
+    languages: "Shellscript",
+    title: "Monero Injector",
+    description: "Automated stealth crypto miner",
+    cached: "https://github.com/shadowctrl/crypto-miner",
+  },
+
+  {
+    image: "/assets/portfolio.webp",
+    languages: "ReactJS, Html, Css",
+    title: "Portfolio",
+    description: "Portfolio Website",
+    live: "https://shadowctrl.me",
+  },
+
+  {
+    image: "/assets/discordjs.webp",
+    languages: "NodeJS, MongoDB, Discord API",
+    title: "Discord Application",
+    description: "Ticketing System",
+    cached: "https://github.com/muralianand12345/Discord-Ticket-Bot",
+  },
+];
+
+export const onGoingProjects = [
+  {
+    image: "/assets/shadowx.webp",
+    languages: "ReactJS, NodeJS, Tailwind CSS, Lottie",
+    title: "ShadowX Website",
+    description: "A startup website design",
+    progress: "https://github.com/shadowctrl/ShadowX",
+  },
+];
+
+export default projects;
+// export { onGoingProjects };
