794b4f9fee4c940846ce9442a0fc07e069c194b1
shadowctrl <shadowctrl@outlook.com>
2024-03-15 02:48:06 +0530

    GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import Navbar from "../components/navbar/navbar.jsx";
 import Footer from "../components/footer/footer.jsx";
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-6LSJFNRLP1" />
     </html>
   );
 }
