09283074b1a6217e2575fc10f210b83d50f3f48d
shadowctrl <shadowctrl@outlook.com>
2024-03-15 11:42:02 +0530

    Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Navbar from "../components/navbar/navbar.jsx";
 import Footer from "../components/footer/footer.jsx";
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         <Footer />
       </body>
       <GoogleAnalytics gaId="G-6LSJFNRLP1" />
+      <Analytics />
     </html>
   );
 }
