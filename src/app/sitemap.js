c8cff1f1abcc6161c865865ce0af85a22d1e3a9e
Shadowctrl <shadowctrl@outlook.com>
2024-10-06 15:57:36 +0530

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: "https://shadowctrl.me/home",
+      url: "https://shadowctrl.me/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
@@ -15,20 +15,20 @@ export default function sitemap() {
     {
       url: "https://shadowctrl.me/services",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "yearly",
       priority: 0.8,
     },
     {
       url: "https://shadowctrl.me/aboutme",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
+      changeFrequency: "yearly",
+      priority: 0.5,
     },
     {
       url: "https://shadowctrl.me/contact",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
+      changeFrequency: "yearly",
+      priority: 0.5,
     },
   ];
 }
