f434b03be7a21e9c7a466e66749c56e89188d665
Shadowctrl <shadowctrl@outlook.com>
2024-10-14 22:57:37 +0530

    "Added new 'Project Catalog' section to services page with image, description, and link, and updated corresponding CSS styles."

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -3,6 +3,7 @@ import CodeIcon from "@mui/icons-material/Code";
 import SecurityIcon from "@mui/icons-material/Security";
 import ApiSharpIcon from "@mui/icons-material/ApiSharp";
 import Link from "next/link";
+import Image from "next/image";
 
 export const metadata = {
   title: "shadowctrl | Services Offered",
@@ -66,6 +67,49 @@ const services = () => {
           <div class="serviceDesktop">{IconSecurity()}</div>
           <br />
           <span>Code Maintenance</span>
+        </div>
+      </div>
+      <div className="service-projects">
+        <div class="serviceHead">
+          <span>#</span>Project Catalog
+          <div class="serviceDash"></div>
+        </div>
+        <h2 className="service-projects-head">
+          Buy Custom Website Programming services, priced and ready to go.
+        </h2>
+        <div className="service-project-container">
+          <p className="service-project-price">From $99</p>
+          <Image
+            src={"/images/nextjs-reactjs-upwork-shadowctrl.jpg"}
+            width={350}
+            height={750}
+          />
+
+          <h2 className="service-project-container-headline">
+            You will get a Custom Full-stack Website | Next.js | Auth | Firebase
+            | API & more
+          </h2>
+
+          <p className="service-project-container-desc">
+            ⭐FOR CLIENTS: Each client has custom requirements, So let's have a
+            chat to decide project requirements, budget and delivery time before
+            buying. Please feel free to contact. <br />
+            <br /> I offer Next.js/React.js services, ranging from simple
+            single-page websites to complex applications. I also specialize in
+            API integration, authentication, payment gateways, Responsive
+            designs and more. <br /> <br />
+            Please feel free to contact regarding any information you may need.
+          </p>
+
+          <Link
+            className="service-project-container-button"
+            target="_blank"
+            href={
+              "https://www.upwork.com/services/product/development-it-next-js-website-all-in-one-customization-pack-1818179802536424028?ref=project_share"
+            }
+          >
+            Continue
+          </Link>
         </div>
       </div>
       <div className="service-contact">
