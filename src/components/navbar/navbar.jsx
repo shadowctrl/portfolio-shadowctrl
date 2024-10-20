6da734b0335a80afe4237f353c6fcab0ace7ded6
Shadowctrl <shadowctrl@outlook.com>
2024-10-20 13:55:59 +0530

    navbar update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,5 @@
 import "./navbar.css";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
 import union from "../../assets/Union.svg";
-import thm from "../../assets/thm.svg";
 import items from "./components";
 import Link from "next/link";
 import Image from "next/image";
@@ -10,29 +7,9 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <div className="navbarParent">
-      <div className="navbarLine"></div>
-      <div className="navbarSocials">
-        <Link href="https://instagram.com/_.shadowctrl._" target="_blank">
-          <InstagramIcon
-            className="navbarSocialItem1"
-            style={{ fontSize: "1.8vw" }}
-          />
-        </Link>
-
-        <Link href="https://github.com/shadowctrl" target="_blank">
-          <GitHubIcon
-            className="navbarSocialItem2"
-            style={{ fontSize: "1.8vw" }}
-          />
-        </Link>
-
-        <Link href="https://tryhackme.com/p/shadowctrl" target="_blank">
-          <Image alt="Try Hack Me" className="navbarSocialItem3" src={thm} />
-        </Link>
-      </div>
       <Link href="/">
         <div className="navbarImg">
-          <Image src={union} alt="Logo Union" />
+          <Image src={union} alt="Logo Union" width={18} height={14} />
           <h1> shadowctrl </h1>
         </div>
       </Link>
