9bf5d229f208fb70d1667f16015bf5f0ab76a1e1
Shadowctrl <shadowctrl@outlook.com>
2024-10-20 20:07:32 +0530

    "Updated contact page CSS and JSX: added new styles and components, including a contact form and social media links."

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import "./contactme.css";
-import { BsDiscord } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
+import { IoChatbubblesOutline } from "react-icons/io5";
+import { RiDiscordLine } from "react-icons/ri";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+  FaSquareUpwork,
+} from "react-icons/fa6";
+import { BsTwitterX } from "react-icons/bs";
 
 export const metadata = {
   title: "Contact Me | shadowctrl",
@@ -9,30 +17,81 @@ export const metadata = {
     "Have a project in mind? Let's discuss!, using the options below or reach out for a coffee chat.",
 };
 
-const IconDiscord = (size = "1.3vw", pr = "0.5vw", pb = "1vh") => {
-  return (
-    <>
-      <BsDiscord
-        size={size}
-        style={{
-          verticalAlign: "middle",
-          paddingRight: pr,
-          paddingBottom: pb,
-        }}
-      />
-      Raghav#4530
-    </>
-  );
-};
-
 const Contactme = () => {
   return (
     <div className="contactparent">
       <div className="contacthead">
-        <span>#</span>Contacts
+        <span>/</span>Contact
         <div className="contactdash"></div>
       </div>
+      <div className="contact-container">
+        <div className="contact-left">
+          <div className="contact-left-top">
+            <div className="contact-options">
+              <div className="contact-options-item">
+                <Link href="mailto:admin@shadowctrl.me" target="_blank">
+                  <IoChatbubblesOutline className="contact-icon" />
+                </Link>
+                Chat
+              </div>
+              <p>Our friendly team is here to help!</p>
+            </div>
+            <div className="contact-options">
+              <div className="contact-options-item">
+                <Link href="https://discord.gg/vJ8b9AB8G2" target="_blank">
+                  <RiDiscordLine className="contact-icon" />{" "}
+                </Link>
+                Discord Community
+              </div>
+              <p>Join the Conversation</p>
+            </div>
+          </div>
 
+          <div className="contact-social-icons">
+            <Link href={"https://facebook.com/shadwoctrl"} target="_blank">
+              <FaFacebook />
+            </Link>
+            <Link href="https://x.com/_shadowctrl_" target="_blank">
+              <BsTwitterX />
+            </Link>
+            <Link href="https://linkedin.com/in/shadowctrl" target="_blank">
+              <FaLinkedin />
+            </Link>
+            <Link href="https://youtube.com/@shadowctrl" target="_blank">
+              <FaYoutube />
+            </Link>
+            <Link href="https://instagram.com/_.shadowctrl._" target="_blank">
+              <FaInstagram />
+            </Link>
+            <Link
+              href="https://upwork.com/freelancers/shadowctrl"
+              target="_blank"
+            >
+              <FaSquareUpwork />
+            </Link>
+          </div>
+        </div>
+        <div className="contact-right">
+          <div>
+            <h1>Got ideas? We've got the skills. Let's team up.</h1>
+            <p>Tell us more about yourself and what you got in mind.</p>
+          </div>
+
+          <form
+            action="
+          "
+          >
+            <input type="text" placeholder="Your name" />
+            <input type="text" placeholder="you@company.com" />
+            <div className="form-desc">
+              <label htmlFor="desc">Tell us little about the project...</label>
+              <br />
+              <input type="text" id="desc" />
+            </div>
+            <button type="submit">Let's get Started</button>
+          </form>
+        </div>
+      </div>
       <div className="contact">
         <p>
           {`I'm`} available for freelance and part-time opportunities. If you
