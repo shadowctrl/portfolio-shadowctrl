c921f8422337185a10c60a2da36f4fabda275b30
shadowctrl <admin@shadowctrl.me>
2025-07-19 22:34:36 +0530

    updated sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,40 +1,34 @@
 export default function sitemap() {
-  return [
-    {
-      url: "https://shadowctrl.me/",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
-    },
-    {
-      url: "https://shadowctrl.me/services",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.9,
-    },
-    {
-      url: "https://shadowctrl.me/works",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://shadowctrl.me/quickpay",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: "https://shadowctrl.me/aboutme",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.6,
-    },
-    {
-      url: "https://shadowctrl.me/contact",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
-    },
-  ];
+	return [
+		{
+			url: 'https://shadowctrl.me/',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+		{
+			url: 'https://shadowctrl.me/services',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 0.9,
+		},
+		{
+			url: 'https://shadowctrl.me/portfolio',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://shadowctrl.me/about',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 0.6,
+		},
+		{
+			url: 'https://shadowctrl.me/contact',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 0.5,
+		},
+	];
 }
