6dae9b2599cea1a8529ea390f6f792bf0a9d59aa
shadowctrl <shadowctrl@outlook.com>
2025-07-19 22:09:20 +0530

    Removed old Header component

--- src/components/navbar/components.js
+++
@@ -1,8 +0,0 @@
-const items = [
-  { name: "works", path: "/works" },
-  { name: "services", path: "/services" },
-  { name: "aboutme", path: "/aboutme" },
-  { name: "contact", path: "/contact" },
-  { name: "quick pay", path: "/quickpay" },
-];
-export default items;
